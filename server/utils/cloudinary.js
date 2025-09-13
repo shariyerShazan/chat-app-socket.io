4996c8d789c18f3762a6148ba8eb7cf861707716
shariyerShazan <shariyershazan1@gmail.com>
2025-09-14 03:55:21 +0600

    cloudinary set

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -11,3 +11,29 @@ cloudinary.config({
 })
 
 export default cloudinary
+
+export const uploadPhoto = (filePathOrUrl, folder = "photos") => {
+    return new Promise((resolve, reject) => {
+      cloudinary.uploader.upload(filePathOrUrl, { folder }, (error, result) => {
+        if (error) return reject(error);
+        resolve(result); 
+      });
+    });
+  };
+  
+  export const deletePhoto = async (photoUrlOrPublicId) => {
+    try {
+      let publicId = photoUrlOrPublicId;
+  
+      if (photoUrlOrPublicId.startsWith("http")) {
+        const parts = photoUrlOrPublicId.split("/");
+        const fileWithExt = parts[parts.length - 1]; 
+        publicId = fileWithExt.substring(0, fileWithExt.lastIndexOf(".")); 
+      }
+  
+      const result = await cloudinary.uploader.destroy(publicId);
+      return result; 
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  };
