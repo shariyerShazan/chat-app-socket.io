b2d6cd646ccd1210e8cb8fab479b2aadbe436817
shariyerShazan <shariyershazan1@gmail.com>
2025-09-14 04:15:16 +0600

    message model schema

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,13 @@
 import express from "express"
 import { login, logout, register } from "../controllers/user.controller.js"
+import { isAuthenticated } from "../middlewares/isAuthenticated.js"
+import { upload } from "../utils/multer.js"
 
 const route = express.Router()
 
 route.post("/register" , register)
 route.post("/login" , login)
 route.post("/logout" , logout)
+route.post("/update-profile" , isAuthenticated , upload.single("profilePicture") , logout)
 
 export default route
