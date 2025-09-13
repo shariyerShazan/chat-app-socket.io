f232021d129c76d180404af8c6badbdf2627faad
shariyerShazan <shariyershazan1@gmail.com>
2025-09-14 04:31:16 +0600

    get messages controller

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import { Message } from "../models/message.model.js"
 import { User } from "../models/user.model.js"
 
 export const getOtherUser = async (req , res)=>{
@@ -13,6 +14,44 @@ export const getOtherUser = async (req , res)=>{
             message : "Other users here",
             success: true
         })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            message : "Internal server error" ,
+            success: fales
+        })
+    }
+}
+
+
+export const getMessage = async (req , res)=>{
+    try {
+         const {userToChatId} = req.body
+         const userToChat = await User.findById(userToChatId)
+         if(!userToChat){
+            return res.status(404).json({
+                message : "User not found" ,
+                success: false 
+            })
+         }
+         const messages = await Message.find({
+            $or:[
+                {senderId: req.userId , reciverId: userToChatId} ,
+                {senderId: userToChatId , reciverId: req.userId } ,
+
+            ]
+         })
+         if(messages.length === 0){
+            return res.status(400).json({
+                messages : "Message not found" ,
+                success: false
+            })
+         }
+         return res.status(200).json({
+            message: "here is meessage" ,
+            success: true ,
+            messages
+         })
     } catch (error) {
         console.log(error)
         return res.status(500).json({
