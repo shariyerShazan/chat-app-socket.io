3431cf58dce93739cd43f1a56a039dba94138f5f
shariyerShazan <shariyershazan1@gmail.com>
2025-09-14 03:09:33 +0600

    user schema added

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ import dotenv from "dotenv"
 dotenv.config()
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import connectDB from "./utils/connectDB.js"
 
 const app = express()
 
+// middlewares
 app.use(cookieParser)
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
@@ -15,9 +17,30 @@ app.use(cors({
 }))
 
 
-const PORT = process.env.PORT
+
+// routes
+app.get("/" , (_ , res)=>{
+    try {
+        res.status(200).json({
+            message: "It's server home page" ,
+            success: true
+        })
+    } catch (error) {
+        console.log(error)
+       return res.status(500).json({
+            message : "Internal server error" ,
+            success: false
+        })
+    }
+})
+// api's
+
+
+// servers
+const PORT = process.env.PORT || 7005
 const runServer = async ()=>{
      try {
+        await connectDB()
         app.listen(PORT , ()=>{
             console.log(`Your server is running at https://localhost:${PORT}`)
         })
@@ -25,3 +48,4 @@ const runServer = async ()=>{
         console.log(error)
      }
 }
+runServer()
---
+++ server/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email : {
+        type : String ,
+        required: true ,
+        unique: true
+    },
+    fullName : {
+        type: String ,
+        required: true 
+    },
+    password: {
+        type: String ,
+        required: true
+    },
+    profilePicture: {
+        type: String ,
+        default: "https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg"
+    }
+} , {timestamps: true})
+
+export const User = mongoose.model("User" , userSchema)
