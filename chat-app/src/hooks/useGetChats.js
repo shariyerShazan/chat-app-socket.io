c2d47789d3abcf3efd891bc181a2454c33cbad28
shariyerShazan <shariyershazan1@gmail.com>
2025-09-14 17:16:26 +0600

    --

--- chat-app/src/hooks/useGetChats.js
+++ chat-app/src/hooks/useGetChats.js
@@ -1,25 +1,36 @@
-import axios from 'axios'
-import React, { useCallback, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { MESSAGE_API_ENDPOINT } from '../utils/apiEndpoints'
-import { setChats } from '../redux/chatSlice'
+import axios from "axios";
+import { useState, useCallback, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { MESSAGE_API_ENDPOINT } from "../utils/apiEndpoints";
+import { setChats } from "../redux/chatSlice";
 
-export const useGetChats = () => {
-    const dispatch = useDispatch()
-         const fetchChats = useCallback(async ()=>{
-                try {
-                    const res = await axios.get(`${MESSAGE_API_ENDPOINT}/get-message` , {withCredentials: true})
-                    if(res.data.success){
-                        dispatch(setChats(res.data.chats))
-                    }
-                } catch (error) {
-                    console.log(error)
-                }
-         }, [dispatch])
-         useEffect(()=>{
-            fetchChats()
-         }, [fetchChats])
-         return {refetchChats : }
-}
+export const useGetChats = (reciverId) => {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false); // loading state
+  const [error, setError] = useState(null); // error state
 
+  const fetchChats = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const res = await axios.get(
+        `${MESSAGE_API_ENDPOINT}/get-message/${reciverId}`,
+        { withCredentials: true }
+      );
+      if (res.data.success) {
+        dispatch(setChats(res.data.chats));
+      }
+    } catch (err) {
+      console.error(err);
+      setError(err.response?.data?.message || "Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  }, [dispatch, reciverId]);
 
+  useEffect(() => {
+    fetchChats();
+  }, [fetchChats]);
+
+  return { refetchChats: fetchChats, loading, error };
+};
--- chat-app/src/hooks/useGetOtherUsers.js
+++ chat-app/src/hooks/useGetOtherUsers.js
@@ -1,26 +1,36 @@
-import axios from 'axios'
-import React, { useCallback, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { MESSAGE_API_ENDPOINT, USER_API_ENDPOINT } from '../utils/apiEndpoints'
-import { setOtherUsers } from '../redux/userSlice'
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { MESSAGE_API_ENDPOINT } from "../utils/apiEndpoints";
+import { setOtherUsers } from "../redux/userSlice";
 
 export const useGetOtherUsers = () => {
-    const dispatch = useDispatch()
-       const fetchOtherUsers = useCallback(async ()=>{
-            try {
-                const res = axios.get(`${MESSAGE_API_ENDPOINT}/other-users` , {withCredentials: true})
-                if(res.data.success){
-                    dispatch(setOtherUsers(res.data.otherUsers))
-                }
-            } catch (error) {
-                console.log(error)
-            }
-       }, [dispatch])
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false); // loading state
+  const [error, setError] = useState(null); // error state
 
-       useEffect(()=>{
-        fetchOtherUsers()
-       }, [fetchOtherUsers])
-       return { refetchOtherUsers :fetchOtherUsers} 
-}
+  const fetchOtherUsers = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const res = await axios.get(`${MESSAGE_API_ENDPOINT}/other-users`, {
+        withCredentials: true,
+      });
 
+      if (res.data.success) {
+        dispatch(setOtherUsers(res.data.otherUsers));
+      }
+    } catch (err) {
+      console.error(err);
+      setError(err.message || "Something went wrong!");
+    } finally {
+      setLoading(false);
+    }
+  }, [dispatch]);
 
+  useEffect(() => {
+    fetchOtherUsers();
+  }, [fetchOtherUsers]);
+
+  return { loading, error, refetchOtherUsers: fetchOtherUsers };
+};
