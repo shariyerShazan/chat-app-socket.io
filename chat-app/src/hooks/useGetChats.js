d4a0ad0b0fded623f04c17497f41cc316b5ab1cf
shariyerShazan <shariyershazan1@gmail.com>
2025-09-14 17:11:17 +0600

    getChats hook make

--- chat-app/src/hooks/useGetChats.js
+++ chat-app/src/hooks/useGetChats.js
@@ -1,1 +1,25 @@
+import axios from 'axios'
+import React, { useCallback, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { MESSAGE_API_ENDPOINT } from '../utils/apiEndpoints'
+import { setChats } from '../redux/chatSlice'
+
+export const useGetChats = () => {
+    const dispatch = useDispatch()
+         const fetchChats = useCallback(async ()=>{
+                try {
+                    const res = await axios.get(`${MESSAGE_API_ENDPOINT}/get-message` , {withCredentials: true})
+                    if(res.data.success){
+                        dispatch(setChats(res.data.chats))
+                    }
+                } catch (error) {
+                    console.log(error)
+                }
+         }, [dispatch])
+         useEffect(()=>{
+            fetchChats()
+         }, [fetchChats])
+         return {refetchChats : }
+}
+
 
