8a03b3d4c954838b55822cad2044bb5c33302f08
shariyerShazan <shariyershazan1@gmail.com>
2025-09-14 06:53:15 +0600

    use redux with redux-persist

--- chat-app/src/main.jsx
+++ chat-app/src/main.jsx
@@ -4,11 +4,21 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import { Routes } from './routes/Routes'
 import { ToastContainer } from 'react-toastify'
+import { PersistGate } from 'redux-persist/integration/react'
+import {persistStore} from "redux-persist";
+import { Provider } from 'react-redux'
+import store from './redux/reduxStore'
 
+let persist = persistStore(store)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={Routes} />
-    <ToastContainer />
+    <Provider store={store} >
+        <PersistGate  loading={null} persistor={persist}>
+              <RouterProvider router={Routes} />
+              <ToastContainer />
+        </PersistGate>
+    </Provider>
+
   </StrictMode>,
 )
